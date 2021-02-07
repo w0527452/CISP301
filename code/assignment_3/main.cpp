954ca71bdc4a7e314afcdd00b7fe8efc7a7d3be9
w0527452 <w0527452@apps.losrios.edu>
2021-02-06 16:35:32 -0800

    Simplify

--- code/assignment_3/main.cpp
+++ code/assignment_3/main.cpp
@@ -4,11 +4,14 @@
 
 /**
  * Get user to input value for bill(name)
+ * Simplify by reducing to single param
  * @param name
  * @param val
  * @return
  */
-float getInput(std::string name, float val) {
+float getInput(std::string name) {
+
+    float val;
 
     std::cout << std::endl << "Enter your monthly cost for " << name << ": ";
     std::cin >> val;
@@ -27,26 +30,30 @@ float getAnnualCost(float monthly) {
 
 int main() {
 
-//    float payment, insurance, gas, oil, tires, maintenance, total;
-//    std::string items[] = {"payment", "insurance", "gas", "oil", "tires", "maintenance"};
-//    std::set<std::string> items = {"payment", "insurance", "gas", "oil", "tires", "maintenance"};
+    std::string items[] = {"loan payment", "insurance", "gas", "oil", "tires", "maintenance"};
 
     float total = 0;
 
-    // C++ version of PHP associative array
-    std::map<std::string, float> bills{
-            {"loan payment", 0},
-            {"insurance",    0},
-            {"gas",          0},
-            {"oil",          0},
-            {"tires",        0},
-            {"maintenance",  0}
-    };
+//    // C++ version of PHP associative array
+//    std::map<std::string, float> bills{
+//            {"loan payment", 0},
+//            {"insurance",    0},
+//            {"gas",          0},
+//            {"oil",          0},
+//            {"tires",        0},
+//            {"maintenance",  0}
+//    };
+//
+//    // Loop through each map pair
+//    for (auto const &bill : bills) {
+//        // increment total by user input for each bill
+//        total += getInput(bill.first, bill.second);
+//    }
 
-    // Loop through each map pair
-    for (auto const &bill : bills) {
-        // increment total by user input for each bill
-        total += getInput(bill.first, bill.second);
+    // Loop through array of strings incrementing total by user-entered amount for each
+    // No need to pass by ref since we're not changing it
+    for ( std::string item:items) {
+        total += getInput(item);
     }
 
     // Set up for decimal output
