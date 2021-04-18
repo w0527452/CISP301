a8a75425b117f7f2ce4e89b546cbb99a27303568
w0527452 <w0527452@apps.losrios.edu>
2021-04-18 14:36:38 -0700

    More in line with instructions

--- code/assignment_12/main.cpp
+++ code/assignment_12/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 /**
  * Mark Chouinard CISP 301 - Chapter 12
@@ -10,7 +12,7 @@ most frequently in the string.
  I went with listing the count of each character due to nearly every string I tested having characters
  that appeared the same number of times.
 
- * Sample Run:
+ * Sample Run using charCount():
 Enter a string: https://chouinard.me
 The character . appears 1 times;
 The character / appears 2 times;
@@ -33,6 +35,55 @@ The character u appears 1 times;
 Process finished with exit code 0
  */
 
+
+
+/**
+ * Find most used letter in string
+ * @param str
+ * @return
+ *
+ * Sample Output
+ * =================
+ * Enter a string: hello world
+ * The character l was used 3 times
+ *
+ * Process finished with exit code 0
+ * ==================
+ * Enter a string: https://chouinard.me
+ * The character / was used 2 times
+ *
+ * Process finished with exit code 0
+ *
+ */
+std::string mostFrequent( std::string str ) {
+    unsigned int counts[256] = {0};
+    unsigned int maxCount = 0;
+    unsigned char maxLetter;
+
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+
+    for (unsigned char x : str) {
+//        std::cout << "x: " << x << std::endl;
+        counts[x]++;
+    }
+
+    for (unsigned int n = 0; n <= 255; n++) {
+        if (counts[n]) {
+            unsigned int times = counts[n];
+            if (times > maxCount ) {
+                maxCount = times;
+                maxLetter = (unsigned char) n;
+            }
+        }
+    }
+
+    std::ostringstream output;
+    output << "The character " << maxLetter << " was used " << maxCount << " times\n";
+
+    return output.str();
+
+}
+
 /**
  * Find the number of times a letter appears in a string
  * I went this route due to multiple letters appearing the same number of times.
@@ -40,7 +91,7 @@ Process finished with exit code 0
  *
  * @param str
  */
-void mostFrequent(std::string str) {
+std::string charCount(std::string str) {
     // declare array of size 256 to account for any ASCII character
     // use value 0 so we can increase it each time a letter is found
     unsigned int counts[256] = {0};
@@ -54,6 +105,7 @@ void mostFrequent(std::string str) {
         counts[(unsigned char) x]++;
     }
 
+    std::ostringstream output;
     // Loop through counts array and output count of each letter found
     for (unsigned int n = 0; n <= 255; n++) {
         if (counts[n]) {
@@ -63,9 +115,14 @@ void mostFrequent(std::string str) {
             // Pull value from array to use as count of this letter
             unsigned int times = counts[n];
             // Output count of this letter
-            std::cout << "The character " << letter << " appears " << times << " times" << std::endl;
+
+            output << "The character " << letter << " appears " << times << " times" << std::endl;
+
+
         }
     }
+
+    return output.str();
 
 }
 
@@ -75,12 +132,15 @@ void mostFrequent(std::string str) {
  * @return
  */
 int main() {
-    std::string userString;
+    std::string userString, countString;
 
     std::cout << "Enter a string: ";
-    std::cin >> userString;
+    std::getline(std::cin, userString);
 
-    mostFrequent(userString);
+//    countString = charCount(userString);
+    countString = mostFrequent(userString);
+
+    std::cout << countString;
 
     return 0;
 }
